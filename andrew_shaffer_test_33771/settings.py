33bddde163ef124c721e4c0ffa73a725c342e2c4
Andrew Shaffer <andrew@citizenstable.co>
2022-02-22 19:20:22 -0800

    Add initial models

--- andrew_shaffer_test_33771/settings.py
+++ andrew_shaffer_test_33771/settings.py
@@ -48,7 +48,8 @@ except (DefaultCredentialsError, PermissionDenied):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env.str("SECRET_KEY")
 
-ALLOWED_HOSTS = env.list("HOST", default=["*"])
+# ALLOWED_HOSTS = env.list("HOST", default=["*"])
+ALLOWED_HOSTS = ['127.0.0.1']
 SITE_ID = 1
 
 SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
--- home/models.py
+++ home/models.py
@@ -1,1 +1,49 @@
+from django.conf import settings
 from django.db import models
+
+
+class App(models.Model):
+    name = models.CharField(max_length=50, blank=False)
+    description = models.TextField()
+    
+    WEB = 'WEB'
+    MOBILE = 'MOBILE'
+    
+    TYPE_CHOICES = (
+        (WEB, 'Web'),
+        (MOBILE, 'Mobile')
+    )
+
+    type = models.CharField(max_length=6, choices=TYPE_CHOICES)
+
+    DJANGO = 'DJANGO'
+    REACT = 'REACT'
+    
+    FRAMEWORK_CHOICES = (
+        (DJANGO, 'Django'),
+        (REACT, 'React Native')
+    )
+    
+    framework = models.CharField(max_length=6, choices=FRAMEWORK_CHOICES)
+
+    domain_name = models.CharField(max_length=50)
+    screenshot = models.URLField()
+    # subscription = models.ForeignKey('Subscription', null=True, on_delete=models.SET_NULL)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='apps', on_delete=models.CASCADE)    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Subscription(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='subscriptions', on_delete=models.CASCADE)
+    plan = models.ForeignKey('Plan', on_delete=models.CASCADE)
+    app = models.ForeignKey('App', on_delete=models.CASCADE)
+    active = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Plan(models.Model):
+    name = models.CharField(max_length=20)
+    description = models.TextField()
+    price = models.DecimalField(decimal_places=0, max_digits=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
